300281d54654fedcf8ce3035304d50ff02eafe8b
jo <yjbkjku@example.com>
2020-05-21 23:23:55 -0500

    again

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
 import schema from './graphql/schema'
+import cors from 'cors'; 
 
 const app = express();
 app.set('port', process.env.PORT || 4000);
+app.use (cors ());
 
 app.use('/graphql', graphqlHTTP({
     graphiql: true,
